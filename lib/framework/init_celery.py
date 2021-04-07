2017c4acfca04ffa7f98bdfe81e7a8d389be2ff6
soju6jan <cybersol@naver.com>
2021-04-08 02:41:04 +0900

    20210408-0241

--- lib/framework/init_celery.py
+++ lib/framework/init_celery.py
@@ -7,22 +7,31 @@ import os
 import sys
 
 from framework import app, logger, path_app_root
-from celery import Celery
-
 try:
-    redis_port = os.environ['REDIS_PORT']
-except:
-    redis_port = '6379'
+    from celery import Celery
 
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:%s/0' % redis_port
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:%s/0' % redis_port
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'])
-#celery.conf.update(app.config)
-celery.conf['CELERY_ENABLE_UTC'] = False
-#celery.conf['CELERY_TIMEZONE'] = 'Asia/Seoul'
-celery.conf.update(
-    task_serializer='pickle',
-    result_serializer='pickle',
-    accept_content=['pickle'],
-    timezone='Asia/Seoul'
-)
+    try:
+        redis_port = os.environ['REDIS_PORT']
+    except:
+        redis_port = '6379'
+
+    app.config['CELERY_BROKER_URL'] = 'redis://localhost:%s/0' % redis_port
+    app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:%s/0' % redis_port
+    celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'])
+    #celery.conf.update(app.config)
+    celery.conf['CELERY_ENABLE_UTC'] = False
+    #celery.conf['CELERY_TIMEZONE'] = 'Asia/Seoul'
+    celery.conf.update(
+        task_serializer='pickle',
+        result_serializer='pickle',
+        accept_content=['pickle'],
+        timezone='Asia/Seoul'
+    )
+
+except:
+    class Celery:
+        def task(original_function):
+            @wraps(original_function)
+            def wrapper_function(*args, **kwargs):  #1
+                return original_function(*args, **kwargs)  #2
+            return wrapper_function
