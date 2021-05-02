e8a59d81eff28f52a9055416ba1a2d8df6e18359
soju6jan <cybersol@naver.com>
2021-05-02 13:41:47 +0900

    20210502-1341

--- lib/system/logic_plugin.py
+++ lib/system/logic_plugin.py
@@ -301,7 +301,9 @@ class LogicPlugin(object):
     @staticmethod
     def custom_plugin_update():
         try:
-            if  os.environ.get('UPDATE_STOP') == 'true':
+            if os.environ.get('UPDATE_STOP') == 'true':
+                return
+            if os.environ.get('PLUGIN_UPDATE_FROM_PYTHON') == 'false':
                 return
             custom_path = os.path.join(path_data, 'custom')
             tmps = os.listdir(custom_path)
