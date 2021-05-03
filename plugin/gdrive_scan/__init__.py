b032b3527ba97f44e247d616f3437bfaaa2ed8e6
soju6jan <cybersol@naver.com>
2021-05-03 22:17:00 +0900

    20210503-2217

--- plugin/gdrive_scan/__init__.py
+++ plugin/gdrive_scan/__init__.py
@@ -17,5 +17,14 @@ except ImportError:
     except:
         pass
 
+try:
+    from sqlitedict import SqliteDict
+except ImportError:
+    try:
+        os.system("{} install sqlitedict".format(app.config['config']['pip']))
+        from sqlitedict import SqliteDict
+    except:
+        pass
+
 from .plugin import blueprint, menu, plugin_load, plugin_unload
 from .logic import Logic
