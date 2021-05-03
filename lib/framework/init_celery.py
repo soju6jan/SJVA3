f4e077c64e31d37c5606e3fbb6fbe2b80a131f0c
soju6jan <cybersol@naver.com>
2021-05-03 18:16:28 +0900

    20210503-1816

--- lib/framework/init_celery.py
+++ lib/framework/init_celery.py
@@ -6,10 +6,6 @@
 import os
 import sys
 
-def is_python3():
-    return sys.version_info >= (3, 0)
-
-
 from framework import app, logger, path_app_root
 try:
     from celery import Celery
@@ -34,33 +30,11 @@ try:
 
 except:
     from functools import wraps
-    from types import FunctionType
-    try:
-        class DummyCelery:
-            def task(**extras):
-                def wrapper(f):
-                    @wraps(f)
-                    def wrapped(*args, **kwargs):
-                        fn_globals = {}
-                        fn_globals.update(globals())
-                        fn_globals.update(extras)
-                        if is_python3():
-                            func_code = '__code__'
-                        else:
-                            func_code = 'func_code'
-                        call_fn = FunctionType(getattr(f, func_code), fn_globals)
-                        return call_fn(*args, **kwargs)
-                    return wrapped
-                return wrapper
-
-
-
-            #def task(self, original_function):
-            #    @wraps(original_function)
-            #    def wrapper_function(*args, **kwargs):  #1
-            #        return original_function(*args, **kwargs)  #2
-            #    return wrapper_function
-        
-        celery = DummyCelery()
-    except:
-        pass
+    class DummyCelery:
+        def task(self, original_function):
+            @wraps(original_function)
+            def wrapper_function(*args, **kwargs):  #1
+                return original_function(*args, **kwargs)  #2
+            return wrapper_function
+    
+    celery = DummyCelery()
