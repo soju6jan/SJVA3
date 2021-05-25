3fcbd3ad10a3866055281d0d257884a5fb5faa6b
soju6jan <cybersol@naver.com>
2021-05-25 16:59:26 +0900

    20210525-1659

--- lib/tool_base/rclone.py
+++ lib/tool_base/rclone.py
@@ -34,3 +34,64 @@ class ToolRclone(object):
             logger.error('Exception:%s', exception)
             logger.error(traceback.format_exc())
     
+
+    @classmethod
+    def getid(cls, remote_path, rclone_path='rclone', config_path=os.path.join(path_data, 'db', 'rclone.conf'), option=None):
+        try:
+            command = [rclone_path, '--config', config_path, 'backend', 'getid', remote_path]
+            if option is not None:
+                command += option
+            ret = ToolSubprocess.execute_command_return(command)
+            if ret is not None and (len(ret.split(' ')) > 1 or ret == ''):
+                ret = None
+            return ret
+        except Exception as exception: 
+            logger.error('Exception:%s', exception)
+            logger.error(traceback.format_exc())
+    
+
+    @classmethod
+    def rmdir(cls, remote_path, rclone_path='rclone', config_path=os.path.join(path_data, 'db', 'rclone.conf'), option=None):
+        try:
+            command = [rclone_path, '--config', config_path, 'rmdir', remote_path, '-vv']#, '--drive-use-trash=false', '-vv']
+            if option is not None:
+                command += option
+            ret = ToolSubprocess.execute_command_return(command)
+            return ret
+        except Exception as exception: 
+            logger.error('Exception:%s', exception)
+            logger.error(traceback.format_exc())
+
+
+    @classmethod
+    def config_list(cls, rclone_path='rclone', config_path=os.path.join(path_data, 'db', 'rclone.conf'), option=None):
+        try:
+            command = [rclone_path, '--config', config_path, 'config', 'dump']#, '--drive-use-trash=false', '-vv']
+            if option is not None:
+                command += option
+            ret = ToolSubprocess.execute_command_return(command, format='json')
+            return ret
+        except Exception as exception: 
+            logger.error('Exception:%s', exception)
+            logger.error(traceback.format_exc())
+
+
+
+
+
+    @classmethod
+    def move_server_side(cls, source, target, rclone_path='rclone', config_path=os.path.join(path_data, 'db', 'rclone.conf'), option=None):
+        try:
+            
+            command = [rclone_path, '--config', config_path, 'move', source, target, '--drive-server-side-across-configs=true', '--delete-empty-src-dirs', '-vv']
+            if option is not None:
+                command += option
+            logger.debug(' '.join(command))
+            ret = ToolSubprocess.execute_command_return(command)
+            return ret
+        except Exception as exception: 
+            logger.error('Exception:%s', exception)
+            logger.error(traceback.format_exc())
+    
+
+            
