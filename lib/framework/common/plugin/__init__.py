2468af138e05472ec0a77da29b0d7753bcb0463a
soju6jan <cybersol@naver.com>
2021-05-25 17:09:15 +0900

    20210525-1709

--- lib/framework/common/plugin/__init__.py
+++ lib/framework/common/plugin/__init__.py
@@ -1,5 +1,1 @@
-from .model_setting import get_model_setting
-from .logic import Logic
-from .route import default_route, default_route_socketio, default_route_single_module
-from .logic_module_base import LogicModuleBase
-from .ffmpeg_queue import FfmpegQueueEntity, FfmpegQueue
+from plugin import get_model_setting, Logic, default_route, default_route_socketio, default_route_single_module, LogicModuleBase, FfmpegQueueEntity, FfmpegQueue
