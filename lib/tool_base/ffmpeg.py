2d95aa8911f6fc0bd794ecc93efc5a7f86cf2bac
soju6jan <cybersol@naver.com>
2021-06-05 20:43:14 +0900

    ffprobe quiet

--- lib/tool_base/ffmpeg.py
+++ lib/tool_base/ffmpeg.py
@@ -9,7 +9,7 @@ class ToolFfmpeg(object):
     @classmethod
     def ffprobe(cls, filepath, ffprobe_path='ffprobe', option=None):
         try:
-            command = [ffprobe_path, '-v', 'error', '-print_format', 'json', '-show_format', '-show_streams', filepath]
+            command = [ffprobe_path, '-v', 'quiet', '-print_format', 'json', '-show_format', '-show_streams', filepath]
             if option is not None:
                 command += option
             logger.warning(' '.join(command))
